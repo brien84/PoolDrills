534aa8eb281376f2f116acc5ede15d522ab40338
brien84 <houseinblues@gmail.com>
2020-03-07 22:18:12 +0200

    Create `Drill` model

---
+++ PoolDrills/Models/Drill.swift
@@ -0,0 +1,13 @@
+//
+//  Drill.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-03-07.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import Foundation
+
+struct Drill {
+    let name: String
+}
