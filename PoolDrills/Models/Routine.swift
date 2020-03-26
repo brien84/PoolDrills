e0614f54929f13c2e3a26cfe6a533b6f98f61166
brien84 <houseinblues@gmail.com>
2020-03-26 20:16:54 +0200

    Add `Routine` model

---
+++ PoolDrills/Models/Routine.swift
@@ -0,0 +1,14 @@
+//
+//  Routine.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-03-26.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import Foundation
+
+struct Routine {
+    let name: String
+    let drills: [Drill]
+}
