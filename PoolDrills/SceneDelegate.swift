1f581d68504978d1b1f6287ebc481aaaabe28047
brien84 <houseinblues@gmail.com>
2020-03-26 20:27:19 +0200

    Add CoreData `Data` model

--- PoolDrills/SceneDelegate.swift
+++ PoolDrills/SceneDelegate.swift
@@ -18,5 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
 }
 
