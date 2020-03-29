16d2e7bce1b92c6f14b4a37fb9edb540890fc7b5
brien84 <houseinblues@gmail.com>
2020-03-29 16:34:20 +0300

    Create `RoutineViewController`

---
+++ PoolDrills/Controllers/RoutineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutineViewController.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-03-29.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+class RoutineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
