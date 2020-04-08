b70c29b727d8398a25d6b75c323ca8548a63da91
brien84 <houseinblues@gmail.com>
2020-04-08 20:07:18 +0300

    Refactor `DrillsViewController` datasource to use `NSFetchedResultsController`

--- PoolDrills/Controllers/DrillsViewController.swift
+++ PoolDrills/Controllers/DrillsViewController.swift
@@ -9,61 +9,71 @@
 import CoreData
 import UIKit
 
-class DrillsViewController: UITableViewController {
+final class DrillsViewController: UITableViewController {
 
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var coreDataStack: CoreDataStack
 
-    private var datasource = [Drill]()
+    private lazy var fetchedResultsController: NSFetchedResultsController<Drill> = {
+        let fetchRequest: NSFetchRequest<Drill> = Drill.fetchRequest()
+
+        let titleSort = NSSortDescriptor(key: #keyPath(Drill.title), ascending: true)
+        fetchRequest.sortDescriptors = [titleSort]
+
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: coreDataStack.managedContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+
+        controller.delegate = self
+
+        return controller
+    }()
+
+
+    init(coreDataStack: CoreDataStack) {
+        self.coreDataStack = coreDataStack
+
+        super.init(style: .plain)
+    }
+
+    required init?(coder: NSCoder) {
+        self.coreDataStack = CoreDataStack()
+
+        super.init(coder: coder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loadData()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        loadData()
-    }
-
-    private func loadData() {
-        let request: NSFetchRequest<Drill> = Drill.fetchRequest()
-
         do {
-            datasource = try context.fetch(request)
+            try fetchedResultsController.performFetch()
         } catch {
             print(error)
         }
-
-        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return datasource.count
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+
+        return section.numberOfObjects
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DrillsCell", for: indexPath)
 
-        cell.textLabel?.text = datasource[indexPath.row].name
+        let drill = fetchedResultsController.object(at: indexPath)
+
+        cell.textLabel?.text = drill.title
 
         return cell
     }
+}
 
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-        if segue.identifier == "DrillViewController" {
-
-            guard let vc = segue.destination as? DrillViewController else { return }
-            guard let cell = sender as? UITableViewCell else { return }
-            guard let indexPath = tableView.indexPath(for: cell) else { return }
-            vc.drill = datasource[indexPath.row]
-        }
+extension DrillsViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
     }
-
 }
