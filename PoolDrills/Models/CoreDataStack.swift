c5faaa719d49246f519e5a4285057e2ec935018d
brien84 <houseinblues@gmail.com>
2020-04-14 23:21:57 +0300

    Add in-memory persistence for tests

--- PoolDrills/Models/CoreDataStack.swift
+++ PoolDrills/Models/CoreDataStack.swift
@@ -9,43 +9,30 @@
 import CoreData
 import Foundation
 
-class CoreDataStack {
+final class CoreDataStack {
 
     private static let modelName = "PoolDrills"
 
-    private static let model: NSManagedObjectModel = {
-        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else { fatalError() }
-        guard let model = NSManagedObjectModel(contentsOf: modelURL) else { fatalError() }
+    private static let storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName)
 
-        return model
-    }()
-
-    private let storeContainer: NSPersistentContainer
-
-    lazy var managedContext: NSManagedObjectContext = {
-        return storeContainer.viewContext
-    }()
-
-    init(persistentStoreType: String = NSSQLiteStoreType) {
-        let persistentStoreDescription = NSPersistentStoreDescription()
-
-        if persistentStoreType == NSSQLiteStoreType {
-            let url = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("\(CoreDataStack.modelName).sqlite")
-            persistentStoreDescription.url = url
+        if ProcessInfo.processInfo.arguments.contains("-TEST") {
+            let persistentStoreDescription = NSPersistentStoreDescription()
+            persistentStoreDescription.type = NSInMemoryStoreType
+            container.persistentStoreDescriptions = [persistentStoreDescription]
         }
-
-        persistentStoreDescription.type = persistentStoreType
-
-        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
-        container.persistentStoreDescriptions = [persistentStoreDescription]
 
         container.loadPersistentStores { storeDescription, error in
             if let error = error as NSError? {
                 print("Unresolved error \(error), \(error.userInfo)")
             }
         }
-   
-        self.storeContainer = container
+
+        return container
+    }()
+
+    var managedContext: NSManagedObjectContext {
+        return CoreDataStack.storeContainer.viewContext
     }
 
     func saveContext () {
