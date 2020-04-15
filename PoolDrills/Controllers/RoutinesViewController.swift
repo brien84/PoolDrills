d320c4fcd2041cdce216aeb504b6f53b1c036eff
brien84 <houseinblues@gmail.com>
2020-04-15 21:03:03 +0300

    Add missing `NSFetchedResultChangeType.move` case in `NSFetchedResultsControllerDelegate` methods

--- PoolDrills/Controllers/RoutinesViewController.swift
+++ PoolDrills/Controllers/RoutinesViewController.swift
@@ -96,7 +96,12 @@ extension RoutinesViewController: NSFetchedResultsControllerDelegate {
         case .update:
             guard let indexPath = indexPath else { return }
             tableView.reloadRows(at: [indexPath], with: .fade)
-        default:
+        case .move:
+            guard let indexPath = indexPath else { return }
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+        @unknown default:
             return
         }
     }
