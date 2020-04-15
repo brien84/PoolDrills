fb31f7101337cb6c0e087a623b39dd41cdd6e2e8
brien84 <houseinblues@gmail.com>
2020-04-15 20:51:46 +0300

    Create `DrillsSelectionViewController`

---
+++ PoolDrills/Controllers/DrillsSelectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DrillsSelectionViewController.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-03-29.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+
+import UIKit
+
+final class DrillsSelectionViewController: UITableViewController {
+
+}
