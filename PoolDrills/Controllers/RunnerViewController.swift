843a66324cc2410608c8c7336f919377679321d7
brien84 <houseinblues@gmail.com>
2020-04-19 18:35:12 +0300

    Connect `RunnerViewController` outlets

--- PoolDrills/Controllers/RunnerViewController.swift
+++ PoolDrills/Controllers/RunnerViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 final class RunnerViewController: UIViewController {
 
+    var routine: Routine?
+
+    @IBOutlet private weak var routineTitle: UILabel!
+    @IBOutlet private weak var time: UILabel!
+    @IBOutlet private weak var activityButton: UIButton!
+    @IBOutlet private weak var missButton: UIButton!
+    @IBOutlet private weak var potButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+
+
 }
