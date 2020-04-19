dc986f9ceeee054a7bf41e9033faae9f30040a4d
brien84 <houseinblues@gmail.com>
2020-04-19 20:04:14 +0300

    Setup `RunnerViewController` action outlets

--- PoolDrills/Controllers/RunnerViewController.swift
+++ PoolDrills/Controllers/RunnerViewController.swift
@@ -12,14 +12,31 @@ final class RunnerViewController: UIViewController {
 
     var routine: Routine?
 
-    @IBOutlet private weak var routineTitle: UILabel!
-    @IBOutlet private weak var time: UILabel!
-    @IBOutlet private weak var activityButton: UIButton!
+    @IBOutlet private weak var drillTitle: UILabel!
+    @IBOutlet private weak var totalTime: UILabel!
+    @IBOutlet private weak var drillTime: UILabel!
+    @IBOutlet private weak var actionButton: UIButton!
     @IBOutlet private weak var missButton: UIButton!
-    @IBOutlet private weak var potButton: UIButton!
+    @IBOutlet private weak var hitButton: UIButton!
+    @IBOutlet private weak var attemptsCount: UILabel!
+
+    @IBAction private func actionButtonDidTap(_ sender: UIButton) {
+
+    }
+
+    @IBAction private func missButtonDidTap(_ sender: UIButton) {
+
+    }
+
+    @IBAction private func hitButtonDidTap(_ sender: UIButton) {
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
+
 
     }
 
