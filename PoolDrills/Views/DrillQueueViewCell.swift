e67295cf0db5994d9e63c2dd967cc50796088c01
brien84 <houseinblues@gmail.com>
2020-05-19 16:47:50 +0300

    Create `DrillQueueViewCell`

---
+++ PoolDrills/Views/DrillQueueViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DrillQueueViewCell.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-05-18.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class DrillQueueViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var duration: UILabel!
+
+    @IBOutlet weak var attempts: UILabel!
+
+}
