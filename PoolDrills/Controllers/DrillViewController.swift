96d9eaada4008f81e0c4e7e9964d77f39910145d
brien84 <houseinblues@gmail.com>
2020-05-22 16:56:49 +0300

    Implement `CustomUISlider` in `DrillViewController`

--- PoolDrills/Controllers/DrillViewController.swift
+++ PoolDrills/Controllers/DrillViewController.swift
@@ -17,30 +17,42 @@ final class DrillViewController: UITableViewController {
     private let coredata = CoreDataStack()
 
     @IBOutlet private weak var titleField: UITextField!
-    @IBOutlet private weak var attemptsSlider: UISlider!
-    @IBOutlet private weak var durationSlider: UISlider!
+    @IBOutlet private weak var attemptsSlider: CustomUISlider!
+    @IBOutlet private weak var durationSlider: CustomUISlider!
 
-    @IBAction private func cancelButtonTapped(_ sender: UIBarButtonItem) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        attemptsSlider.font = titleField.font
+        durationSlider.font = titleField.font
+
+        titleField.text = drill.title
+        attemptsSlider.value = Float(drill.attempts)
+        durationSlider.value = Float(drill.duration / 60)
+    }
+
+    @IBAction private func cancelButtonDidTap(_ sender: UIBarButtonItem) {
         coredata.managedContext.rollback()
 
         navigationController?.popViewController(animated: true)
     }
 
-    @IBAction private func saveButtonTapped(_ sender: UIBarButtonItem) {
+    @IBAction private func saveButtonDidTap(_ sender: UIBarButtonItem) {
         drill.title = titleField.text
         drill.attempts = Int32(attemptsSlider.value)
-        drill.duration = Double(durationSlider.value).rounded(.up)
+        drill.duration = Double(durationSlider.value).rounded(.up) * 60
 
         coredata.saveContext()
 
         navigationController?.popViewController(animated: true)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        titleField.text = drill.title
-        attemptsSlider.value = Float(drill.attempts)
-        durationSlider.value = Float(drill.duration)
+    @IBAction private func attemptsSliderDidChangeValue(_ sender: CustomUISlider) {
+        sender.valueLabel.text = String(Int(sender.value))
     }
+
+    @IBAction private func durationSliderDidChangeValue(_ sender: CustomUISlider) {
+        sender.valueLabel.text = String("\(Int(sender.value)) min")
+    }
+
 }
