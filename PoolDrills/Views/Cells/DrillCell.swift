02ca656a76496878b847b2cfd3ccf1a6dbc95271
brien84 <houseinblues@gmail.com>
2020-05-23 23:30:04 +0300

    Create `DrillCell` class

---
+++ PoolDrills/Views/Cells/DrillCell.swift
@@ -0,0 +1,17 @@
+//
+//  DrillCell.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-05-23.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class DrillCell: CustomSelectedBackgroundCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var attempts: UILabel!
+    @IBOutlet weak var duration: UILabel!
+
+}
