b26039743eaa4966cde0489e154917685b52f216
brien84 <houseinblues@gmail.com>
2020-05-23 23:28:16 +0300

    Create `Extensions` support file

---
+++ PoolDrills/Support/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-05-23.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var primaryBackground: UIColor! {
+        return UIColor(named: "primaryBackground")
+    }
+
+    static var secondaryBackground: UIColor! {
+        return UIColor(named: "secondaryBackground")
+    }
+
+    static var darkElement: UIColor! {
+        return UIColor(named: "darkElement")
+    }
+
+    static var grayElement: UIColor! {
+        return UIColor(named: "grayElement")
+    }
+
+    static var lightElement: UIColor! {
+        return UIColor(named: "lightElement")
+    }
+}
