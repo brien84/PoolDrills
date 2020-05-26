f76bbeb247a5845afd576520d38bf91df3643047
brien84 <houseinblues@gmail.com>
2020-05-26 14:25:43 +0300

    Replace `UIAlertController` implementation with `ConfirmationPopoverViewController`

--- PoolDrills/Controllers/RunnerViewController.swift
+++ PoolDrills/Controllers/RunnerViewController.swift
@@ -65,35 +65,19 @@ final class RunnerViewController: UIViewController {
     }
 
     @IBAction private func backButtonDidTap(_ sender: UIBarButtonItem) {
-        drillTracker.toggle()
+        let alert = ConfirmationPopoverViewController(in: self, on: sender)
 
-        let alert = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .alert)
-
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
-            self.navigationController?.popViewController(animated: true)
-        }))
-
-        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { _ in
-            self.drillTracker.toggle()
-        }))
-
-        self.present(alert, animated: true)
+        alert.present {
+            if $0 { self.navigationController?.popViewController(animated: true) }
+        }
     }
 
     @IBAction private func completionButtonDidTap(_ sender: UIButton) {
-        drillTracker.toggle()
+        let alert = ConfirmationPopoverViewController(in: self, on: sender)
 
-        let alert = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .alert)
-
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
-            self.drillTracker.endDrill()
-        }))
-
-        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { _ in
-            self.drillTracker.toggle()
-        }))
-
-        self.present(alert, animated: true)
+        alert.present {
+            if $0 { self.drillTracker.endDrill() }
+        }
     }
 
     override func viewDidLoad() {
