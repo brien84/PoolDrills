8b619a9a3fb53539a86d5a4babb4f58ff7981511
brien84 <houseinblues@gmail.com>
2020-06-01 20:23:47 +0300

    Create `ResultsViewCell` class

---
+++ PoolDrills/Views/Cells/ResultsViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsViewCell.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-05-31.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class ResultsViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var duration: UILabel!
+    @IBOutlet weak var hitCount: UILabel!
+    @IBOutlet weak var missCount: UILabel!
+
+}
