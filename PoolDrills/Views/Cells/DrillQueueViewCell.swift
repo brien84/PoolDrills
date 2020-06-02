54252f9baad45e357c9e14a4ed39108a6051ae4b
brien84 <houseinblues@gmail.com>
2020-06-02 22:12:26 +0300

    Implement cell border

--- PoolDrills/Views/Cells/DrillQueueViewCell.swift
+++ PoolDrills/Views/Cells/DrillQueueViewCell.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 final class DrillQueueViewCell: UICollectionViewCell {
-    
+
     @IBOutlet weak var title: UILabel!
-
     @IBOutlet weak var duration: UILabel!
-
     @IBOutlet weak var attempts: UILabel!
+
+    override func awakeFromNib() {
+        layer.masksToBounds = true
+        layer.borderColor = UIColor.lightElement.cgColor
+        layer.borderWidth = 2.0
+        layer.cornerRadius = 20
+    }
 
 }
