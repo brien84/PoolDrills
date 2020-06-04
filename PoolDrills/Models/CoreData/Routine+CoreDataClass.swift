c60b119ddaee33fd9b7f7304c204d2c5976a15b6
brien84 <houseinblues@gmail.com>
2020-06-04 12:48:42 +0300

    Generate CoreData subclasses

---
+++ PoolDrills/Models/CoreData/Routine+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Routine+CoreDataClass.swift
+//  PoolDrills
+//
+//  Created by Marius on 2020-06-04.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+//
+
+import CoreData
+import Foundation
+
+public class Routine: NSManagedObject {
+
+}
