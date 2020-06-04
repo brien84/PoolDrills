24e9e671a1b32c7cbe04c742285793ed52f38170
brien84 <houseinblues@gmail.com>
2020-06-04 23:04:41 +0300

    Add wrapper computed properties

--- PoolDrills/Models/CoreData/Drill+CoreDataClass.swift
+++ PoolDrills/Models/CoreData/Drill+CoreDataClass.swift
@@ -12,4 +12,18 @@ import Foundation
 
 public class Drill: NSManagedObject {
 
+    var attempts: Int {
+        get { return Int(attempts64) }
+        set { attempts64 = Int64(newValue) }
+    }
+
+    var minutes: Int {
+        get { return Int(duration) }
+        set { duration = Int64(newValue) }
+    }
+
+    var seconds: TimeInterval {
+        return TimeInterval(minutes * 60)
+    }
+
 }
